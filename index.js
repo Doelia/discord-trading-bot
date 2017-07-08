cd5aa91fad961e045b794972720f502b3a47737a
Doelia <kyxalia@gmail.com>
2017-07-08 23:25:25 +0200

    Add lambo command

--- index.js
+++ index.js
@@ -13,13 +13,11 @@ client.on('message', message => {
 			let n = args[1];
 			if (n) {
 				let calc = (tx) => {
-					let o = (n * tx);
-					o += o * 0.0025;
-					o += n * 0.0025;
+					let o = (n * tx / (0.9975 * 0.9975));
 					return 'SELL ' + o.toFixed(8) + "\n";
 				}
 				let s = "\n";
-				s += "Sells orders avec un achat à " + n + ":\n";
+				s += "**Sells orders avec un achat à " + n + ":**\n";
 				s += '+5% : ' + calc(1.05, n);
 				s += '+3% : ' + calc(1.03, n);
 				s += '+0% : ' + calc(1, n);
@@ -28,6 +26,28 @@ client.on('message', message => {
 				s += '(Commissions bittrex de 0.25% incluses)';
 				message.reply(s);
 			}
+		}
+		if (args[0] == 'lambo') {
+			let n = args[1];
+			if (n) {
+				let calc = (days) => {
+					return Math.floor(n * Math.pow(1.03, days))
+					+ "€\n";
+				}
+				let s = "\n";
+				s += "**À coups de 3% par jours :**\n";
+				s += '- ' + n + "€ dans 1 mois : " + calc(30);
+				s += '- ' + n + "€ dans 3 mois : " + calc(30 * 3);
+				s += '- ' + n + "€ dans 6 mois : " + calc(30 * 6);
+				s += '- ' + n + "€ dans 1 an : " + calc(365);
+				message.reply(s);
+			} else {
+				message.reply("Ok mais combien tu met ?");
+			}
+		}
+		if (args[0] == 'help') {
+			let s = "\n";
+		
 		}
 	}
 });
